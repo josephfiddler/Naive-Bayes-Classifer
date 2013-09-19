2bbd0d853b7fa02cabaf4ca5ffb2b7c573132a66
Joey Fiddler <joseph.fiddler@students.fhu.edu>
2013-09-19 14:18:05 -0500

    Parsing the Data File Works
    I just need to make sure how it works at the moment.

--- NBC.py
+++ NBC.py
@@ -1,4 +1,6 @@
-def parseDataFile(self, dataFile):
+class NaiveBayesClassifer:
+	
+	def parseDataFile(self, dataFile):
 		listofVariables = []
 		listOfInstances = []
 		file = open (dataFile, 'rU')
@@ -18,16 +20,36 @@ def parseDataFile(self, dataFile):
 					if len(listofVariables) < 1:
 						print "Bad Data File"
 						break
-
+					
 					for attr, value in zip(listofVariables, valueList):
 						tempInstance.values[attr] = (value).lower()
 					listOfInstances.append(tempInstance)
 
 					# listOfInstances.append(Instance())
-
+				
 					# valueList = parseLine(line)
 					# for attr,value in zip(listofVariables, valueList):
 					# 	print attr + ":" + value
 					# 	listOfInstances[-1].values[attr] = value
 		
 		return listOfInstances
+		
+	def parseLine(self, line): #parses variables on an @ line
+		wordList = line.split("\t") #put each word into list 
+		return wordList
+		
+	class Instance:
+		values = {} #dictionary entries
+		@property  #the actual classification of the Instance
+		def Classification(self):
+			return self.values['class']
+		def __init__(self):
+			self.values = {}
+		def __repr__(self):
+			return str(self.values) + " ("+ self.Classification + ")"
+		
+if __name__ == '__main__':
+	print "Hello world"
+	nbc = NaiveBayesClassifer()
+	nbc.parseDataFile("/Users/Joey/Desktop/IntroToAI/rbes/data.txt")
+	
