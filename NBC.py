12408f267de24a8ff06f581cfebb2ccf81c41204
Joey Fiddler <joseph.fiddler@students.fhu.edu>
2013-09-26 19:50:53 -0500

    Trying to clean up code and set up test better

--- NBC.py
+++ NBC.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 
 class NaiveBayesClassifer:
+	
+	def Test(self, dataFile):
+		listOfDicts = 
 				
 	def parseDataFile(self, dataFile):
 		listofVariables = []
@@ -33,9 +36,6 @@ class NaiveBayesClassifer:
 					tempInstance = self.Instance()
 					valueList = self.parseLine(line)
 					
-					
-					
-					
 					#print valueList #This prints all the values for those variables
 					if len(listofVariables) < 1:
 						print "Bad Data File"
@@ -51,8 +51,6 @@ class NaiveBayesClassifer:
 					geekInstance.values['prob'] = str(geekCount) + "/" + str(geekCount + nongeekCount)
 					nongeekInstance.values['prob'] = str(nongeekCount) + "/" + str(geekCount + nongeekCount)
 					
-					
-					
 					#From what I can tell, this where the values are being put into the dictionaries.
 					#This function and the Instace class is where we may need a dictionary of dictionaries
 					#like on the Google doc. At least I narrowed down where the problem is. I believe once 					#we get the dictionary thing down, the rest of this program will be easy.
@@ -60,270 +58,28 @@ class NaiveBayesClassifer:
 						tempInstance.values[attr] = (value).lower()
 					listOfInstances.append(tempInstance)
 					
-					for attr in listofVariables:
-						geekInstance.values.setdefault(attr, {})
-					
 					#Defnitely will have to use a Switch statement. Can't think of any other way, it will be huge
 					#This is all test at the moment
-					for attr, value in zip(listofVariables, valueList):
-						if (value == "geek"):
-							for attr2, value2 in zip(listofVariables, valueList):
-								geekDict[value2] += 1
-								
-								if not geekInstance.values.has_key(attr2):
-									geekInstance.values[attr2] = {}
-								if not geekInstance.values[attr2].has_key(value2):
-									geekInstance.values[attr2][value2] = 1
-								else:
-									geekInstance.values[attr2][value2] += 1
-								
-						elif (value == "non-geek"):
-							for attr2, value2 in zip(listofVariables, valueList):
-								nongeekDict[value2] += 1
-								#nonGeekProbs[attr2].update(nongeekDict)
-								if not nongeekInstance.values.has_key(attr2):
-									nongeekInstance.values[attr2] = {}
-								if not nongeekInstance.values[attr2].has_key(value2):
-									nongeekInstance.values[attr2][value2] = 1
-								else:
-									nongeekInstance.values[attr2][value2] += 1
-								#nongeekInstance.values[attr2][value2] = nongeekDict[value2]
-									#if not nongeekInstance.values[attr2].has_key(value2):
-										#nongeekInstance.values[attr2][value2] = {}
-								#nongeekInstance.values[attr2][value2] = nongeekDict[value2]
-								#nongeekInstance.values[attr2][value2] = nongeekDict[value2]
-								
-								'''if (attr2 == "@gpa"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-									break'''
-								'''if (attr2 == "@gender"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@education"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-									break
-								elif (attr2 == "@relationship_status"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@major"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@age"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@employment"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@ACT"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@eyesight"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@subject"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@sports_cards"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@magic"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@comic_books"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@action_figures"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@transformers"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@board_games"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@video_games"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@computer_games"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@d&d"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@sports"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@bacon"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@zombies"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@pokemon"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@anime"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@game_system"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@legos"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@remote"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@camping"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@desktop_OS"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@mobile_OS"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@browser"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@stackoverflow"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@lifehacker"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@slashdot"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@hacker_news"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@quora"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@xkcd"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@ccpp"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@java"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@obj_c"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@php"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@c_sharp"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@python"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@perl"):
-									nongeekDict[value2] += 1
-									#nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@javascript"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@lisp"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@vb_net"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@bash"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@ruby"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@star_wars_4_6"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@star_wars_1_3"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@lotr"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@matrix"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@tron1982"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@tron2010"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@spaceballs"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@blade_runner"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@xfiles"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@fringe"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@it_crowd"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@mythbusters"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@numb3rs"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@dr_who_2005"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@dr_who_1963"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@battlestar_galactica_2004"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@battlestar_galactica_1978"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@star_trek"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@wars_trek"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@lotr_ee"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)
-								elif (attr2 == "@lotr_novels"):
-									nongeekDict[value2] += 1
-									nongeekInstance.values.setdefault(attr2, {}).update(nongeekDict)'''
-									
-								
-						nongeekDict.clear()
-						#nongeekInstance.values.setdefault('@gpa', {}).update(nongeekDict.fromkeys(nongeekDict.keys()))
-						
+					if (tempInstance.Classification == "geek"):
+						for attr2, value2 in zip(listofVariables, valueList):
+							if not geekInstance.values.has_key(attr2):
+								geekInstance.values[attr2] = {}
+							if not geekInstance.values[attr2].has_key(value2):
+								geekInstance.values[attr2][value2] = 1
+							else:
+								geekInstance.values[attr2][value2] += 1
+					elif (tempInstance.Classification == "non-geek"):
+						for attr2, value2 in zip(listofVariables, valueList):
+							if not nongeekInstance.values.has_key(attr2):
+								nongeekInstance.values[attr2] = {}
+							if not nongeekInstance.values[attr2].has_key(value2):
+								nongeekInstance.values[attr2][value2] = 1
+							else:
+								nongeekInstance.values[attr2][value2] += 1
 							
-					
-
-					# listOfInstances.append(Instance())
-				
-					# valueList = parseLine(line)
-					# for attr,value in zip(listofVariables, valueList):
-					# 	print attr + ":" + value
-					# 	listOfInstances[-1].values[attr] = value
+					nongeekDict.clear()
 		
 		
-		#print geekDict.items()
-		#nongeekInstance.values.update(nongeekDict)
-		#print nongeekDict.items()
 		print nongeekInstance.values
 		print geekInstance.values
 		
